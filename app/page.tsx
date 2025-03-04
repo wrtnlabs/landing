f8a7adeab56330f8a63ce6bc102987388b44e577
Amber <hyeyoenchurros@naver.com>
2025-03-04 16:43:58 +0900

    feat: add prettier plugin

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
       <header></header>
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
         hello
       </main>
       <footer></footer>
