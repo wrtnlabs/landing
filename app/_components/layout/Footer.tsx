79bc617cb4f34e2c8605a62bab9b8b79299eb341
Amber <hyeyoenchurros@naver.com>
2025-03-11 16:39:01 +0900

    feat: add footer

--- app/_components/layout/Footer.tsx
+++ app/_components/layout/Footer.tsx
@@ -1,18 +1,46 @@
+import clsx from "clsx";
+import { HamIcon } from "lucide-react";
 import Link from "next/link";
+
+const sectionTitleStyle = clsx(["text-zinc-600"]);
+const linkStyle = clsx(["text-zinc-200"]);
+
+const FOOTER_CONTENTS = [
+  {
+    title: "Developers",
+    links: [
+      { label: "Github", href: "https://github.com/wrtnlabs" },
+      { label: "Docs", href: "https://wrtnlabs.io/blog-overview" },
+    ],
+  },
+  {
+    title: "Company",
+    links: [{ label: "Wrtn Labs", href: "https://wrtnlabs.io/" }],
+  },
+];
 
 export default function Footer() {
   return (
-    <footer className="flex justify-end gap-16 bg-gray-800 px-16 py-8">
-      <nav className="flex flex-col gap-1">
-        Developer
-        <Link href="https://github.com/wrtnlabs">Github</Link>
-        <Link href="/blog">Docs</Link>
-      </nav>
-      <nav className="flex flex-col gap-1">
-        Company
-        <Link href="https://wrtnlabs.io/">Wrtn Labs</Link>
-        <Link href="https://wrtn.io/">wrtn.io</Link>
-      </nav>
+    <footer className="flex h-64 justify-between bg-zinc-900 px-16 py-8">
+      {/* MEMO: Icon Change @Amber */}
+      <HamIcon color="#ffffff" size={72} />
+      <div className="flex gap-16">
+        {FOOTER_CONTENTS.map(({ title, links }) => (
+          <nav key={title} className="flex flex-col gap-1">
+            <p className={sectionTitleStyle}>{title}</p>
+            {links.map(({ label, href }) => (
+              <Link
+                key={label}
+                href={href}
+                target="_blank"
+                className={linkStyle}
+              >
+                {label}
+              </Link>
+            ))}
+          </nav>
+        ))}
+      </div>
     </footer>
   );
 }
