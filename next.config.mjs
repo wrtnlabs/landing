3a7fabc2fb79e5fe7f966a217d3d1f949ad7f5e1
Amber <81298254+syhgpd@users.noreply.github.com>
2025-04-08 16:50:15 +0900

    feat: gh-pages (#27)

--- next.config.mjs
+++ next.config.mjs
@@ -7,4 +7,9 @@ const withNextra = nextra({
 // You can include other Next.js configuration options here, in addition to Nextra settings:
 export default withNextra({
   // ... Other Next.js config options
+  output: "export",
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
 });
