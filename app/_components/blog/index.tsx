6849d19a4c79ed80855f41332e3d062921e1c145
Amber <hyeyoenchurros@naver.com>
2025-04-08 15:42:28 +0900

    feat: blog page

--- app/_components/blog/index.tsx
+++ app/_components/blog/index.tsx
@@ -1,36 +1,48 @@
-import "nextra-theme-blog/style.css";
-
 import { getPosts } from "@/app/_lib/getPost";
+import { getTags } from "@/app/_lib/getTags";
 import Link from "next/link";
 
 export default async function BlogList() {
   const posts = await getPosts();
+  const allTags = await getTags();
+  const tags = [...new Set(allTags)];
 
   return (
-    <div className="mx-auto flex min-h-screen max-w-2xl flex-col gap-6 pt-20">
-      <h1 className="text-3xl">Blog</h1>
-      <div className="flex flex-col items-center gap-3">
-        {posts.map(({ frontMatter, route }) => {
-          const { title, date, category, author } = frontMatter;
-          return (
-            <Link
-              href={`/blog/${route}`}
-              key={route}
-              className="group flex w-full cursor-pointer flex-col gap-2 rounded-xl border border-gray-200 p-4"
-            >
-              <div className="w-fit rounded-sm bg-gray-100 px-2 py-1 text-sm">
-                {category}
-              </div>
-              <h1 className="text-lg font-medium text-black">{title}</h1>
-              <div className="flex gap-1 text-sm text-gray-700">
-                <p className="font-semibold">{date}</p>
-                <p>∙</p>
-                <p>{author}</p>
-              </div>
-            </Link>
-          );
-        })}
+    <div className="pt-20 min-h-screen bg-black">
+      <div className="max-w-2xl mx-auto flex-col gap-6 flex">
+        <h1 className="text-3xl text-white">Blog</h1>
+        <div className="flex w-full flex-wrap gap-2">
+          <div className="w-fit rounded-full cursor-pointer bg-gray-100 px-2 py-1 text-sm text-gray-900 min-w-10 text-center">All</div>
+          {tags?.map((tag) => <div key={tag} className="w-fit rounded-full cursor-pointer bg-gray-100 px-2 py-1 text-sm text-gray-900">{tag}</div>)}
+        </div>
+
+        <div className="flex flex-col items-center gap-3">
+          {posts.map(({ frontMatter, route }) => {
+            const { title, date, tags, author } = frontMatter;
+            return (
+              <Link
+                href={`/blog/${route}`}
+                key={route}
+                className="group flex w-full hover:bg-white/20 duration-300 transition-all cursor-pointer flex-col gap-2 rounded-xl border-gray-700 p-4"
+              >
+                <h1 className="h-16 text-2xl font-medium text-zinc-50">
+                  {title}
+                </h1>
+                <div className="flex w-full items-center justify-between">
+                  <div className="flex gap-1 text-base text-gray-300">
+                    <p>{date}</p>
+                    <p>∙</p>
+                    <p>{author}</p>
+                  </div>
+                  <div className="w-fit rounded-full bg-gray-800 px-2 py-1 text-xs text-gray-400 border-[0.5px] border-gray-600">
+                    # {tags}
+                  </div>
+                </div>
+              </Link>
+            )
+          })}
+        </div>
       </div>
-    </div>
+    </div >
   );
 }
