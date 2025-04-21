fd0a7820be17cb5f7fe80181f50549613736da82
Amber <hyeyoenchurros@naver.com>
2025-04-21 16:26:06 +0900

    feat: update header

--- app/_components/layout/Header.tsx
+++ app/_components/layout/Header.tsx
@@ -5,21 +5,20 @@ import { Button } from "../common/button";
 import Image from "next/image";
 import { openToast } from "../common/toast";
 import { addBasePath } from "@/app/_lib/add-base-path";
-export default function Header() {
+
+export default function Header({ isAgentOS = true, isDark = false }: { isAgentOS?: boolean, isDark?: boolean }) {
   return (
     <header className="fixed top-0 z-50 flex h-[70px] w-full items-center justify-between px-4 backdrop-blur-xl">
       <div className="mx-auto flex w-full max-w-[1440px] items-center justify-between">
         <Link href="/">
           <Image
-            src={addBasePath("/images/logo-black.svg")}
+            src={addBasePath(isDark ? "/images/logo-white.svg" : "/images/logo-black.svg")}
             alt="logo"
             width={120}
             height={28}
           />
         </Link>
-        <Button className="hidden md:block" onClick={openToast}>
-          Open Agent OS
-        </Button>
+        {isAgentOS && <Button className="hidden md:block" onClick={openToast}>Open Agent OS</Button>}
       </div>
     </header>
   );
